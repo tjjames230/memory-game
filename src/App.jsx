89d6ba9202f453e84021f19ab38f35fd174cfca5
tjjames230 <108950500+tjjames230@users.noreply.github.com>
2024-11-26 16:04:03 -0600

    update: fixed useEffect to fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -2,45 +2,19 @@ import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Instructions from "./components/Instructions";
 import Card from "./components/Card";
-
-const getImages = async () => {
-	const urlPageOne = "https://swapi.dev/api/people/";
-	const urlPageTwo = "https://swapi.dev/api/people/?page=2";
-	const data = [];
-
-	try {
-		const [response1, response2] = await Promise.all([
-			fetch(urlPageOne),
-			fetch(urlPageTwo),
-		]);
-
-		if (!response1.ok || !response2.ok) {
-			throw new Error(`One or more requests failed`);
-		}
-
-		const people1 = await response1.json();
-		const people2 = await response2.json();
-
-		for (let i = 0; i < people1.results.length; i++) {
-			data.push(people1.results[i]);
-		}
-
-		for (let i = 0; i < 2; i++) {
-			data.push(people2.results[i]);
-		}
-	} catch (error) {
-		console.error(error.message);
-	}
-
-	return data;
-};
+import getImages from "./utils/getImages";
 
 function App() {
 	const [data, setData] = useState(null);
 	const [list, setList] = useState([]);
 
 	useEffect(() => {
-		setData(getImages());
+		const fetchData = async () => {
+			const result = await getImages();
+			setData(result);
+		};
+
+		fetchData();
 	}, []);
 
 	console.log(data);
